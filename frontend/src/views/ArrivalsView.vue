6e71bd9fdc7b3be43f89bb6020c2f6a0b2c8ff69
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-03 11:39:06 +0200

    Added table for departures and arrivals

--- frontend/src/views/ArrivalsView.vue
+++ frontend/src/views/ArrivalsView.vue
@@ -3,5 +3,34 @@
 </script>
 
 <template>
-  <p>Dolasci</p>
+
+  <div id="Departures">
+    <h1 class="h3">Odlasci</h1>
+    <hr>
+    <table class="table table-striped table-hover">
+      <thead>
+      <tr>
+        <th scope="col">Broj leta</th>
+        <th scope="col">Destinacija</th>
+        <th scope="col">Kompanija</th>
+        <th scope="col">Vreme</th>
+        <th scope="col">Info</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Barsa</td>
+        <td>Wiz</td>
+        <td>3.4.2025. 20:00</td>
+        <td>
+          <div>
+            <button type="button" class="btn btn-primary">A</button>
+            <button type="button" class="btn btn-danger">B</button>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
--- frontend/src/views/DeparturesView.vue
+++ frontend/src/views/DeparturesView.vue
@@ -3,6 +3,33 @@
 </script>
 
 <template>
-  <h1>Odlasci</h1>
+
+  <div id="Departures">
+    <h1 class="h3">Dolasci</h1>
+    <hr>
+    <table class="table table-striped table-hover">
+      <thead>
+      <tr>
+        <th scope="col">Broj leta</th>
+        <th scope="col">Destinacija</th>
+        <th scope="col">Kompanija</th>
+        <th scope="col">Vreme</th>
+        <th scope="col">Info</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Barsa</td>
+        <td>Wiz</td>
+        <td>3.4.2025. 20:00</td>
+        <td>
+          <button type="button" class="btn btn-primary">A</button>
+          <button type="button" class="btn btn-danger">B</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
