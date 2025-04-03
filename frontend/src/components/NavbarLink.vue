54af2471a4cfbc13f0c9f720ef3f3c7d3201f05e
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-03 10:14:18 +0200

    Created NavbarLink

---
+++ frontend/src/components/NavbarLink.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+export interface NavbarLinkProps {
+  to: string;
+  name: string;
+}
+
+const props = defineProps<NavbarLinkProps>()
+
+</script>
+
+<template>
+  <li class="nav-item">
+    <RouterLink class="nav-link active" :to="props.to">{{ props.name }}</RouterLink>
+  </li>
+</template>
