ed0750c59f069c7d983d63e5fa0d55c0b520cbe6
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-03 11:43:08 +0200

    Implemented pagination

--- frontend/src/views/DeparturesView.vue
+++ frontend/src/views/DeparturesView.vue
@@ -7,6 +7,23 @@
   <div id="Departures">
     <h1 class="h3">Dolasci</h1>
     <hr>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
     <table class="table table-striped table-hover">
       <thead>
       <tr>
