eb38f20d2de4c1958755cd58d2efde64e1edbd26
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-03 14:08:02 +0200

    Made fixes to tables

--- frontend/src/views/ArrivalsView.vue
+++ frontend/src/views/ArrivalsView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import {ref} from 'vue'
 import axios from "axios";
+import type {PageModel} from "@/models/page.model.ts";
+import type {FlightModel} from "@/models/flight.model.ts";
 
-const flights = ref<any>()
+const flights = ref<PageModel<FlightModel>>()
 axios.get('https://flight.pequla.com/api/flight?type=arrival')
     .then(rsp => flights.value = rsp.data)
 </script>
@@ -10,50 +12,52 @@ axios.get('https://flight.pequla.com/api/flight?type=arrival')
 <template>
 
   <div id="Departures">
-    {{ flights }}
-    <h1 class="h3">Odlasci</h1>
+<!--    {{ flights }}-->
+    <h1 class="container text-center"><i class="fa-solid fa-plane-arrival"></i> Dolasci</h1>
     <hr>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <table class="table table-striped table-hover">
-      <thead>
-      <tr>
-        <th scope="col">Broj leta</th>
-        <th scope="col">Destinacija</th>
-        <th scope="col">Kompanija</th>
-        <th scope="col">Vreme</th>
-        <th scope="col">Info</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Barsa</td>
-        <td>Wiz</td>
-        <td>3.4.2025. 20:00</td>
-        <td>
-          <div>
+    <div class="container" v-if="flights">
+      <table class="table table-striped table-hover text-center">
+        <thead>
+        <tr>
+          <th scope="col">Broj leta</th>
+          <th scope="col">Odredište</th>
+          <th scope="col">Izlaz</th>
+          <th scope="col">Planirano vreme</th>
+          <th scope="col">Procenjeno vreme</th>
+          <th scope="col">Info</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="flight in flights.content" :flight-id="flight.id">
+          <th scope="row">{{ flight.flightNumber }}</th>
+          <td>{{ flight.destination }}</td>
+          <td>{{ (flight.gate)? flight.gate : "N/A" }}</td>
+          <td>{{ new Date(flight.scheduledAt).toLocaleString('sr') }}</td>
+          <td>{{ flight.estimatedAt ? new Date(flight.estimatedAt).toLocaleString('sr') : 'N/A' }}</td>
+          <td>
             <button type="button" class="btn btn-primary">A</button>
             <button type="button" class="btn btn-danger">B</button>
-          </div>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
--- frontend/src/views/DeparturesView.vue
+++ frontend/src/views/DeparturesView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import {ref} from 'vue'
 import axios from "axios";
+import type {PageModel} from "@/models/page.model.ts";
+import type {FlightModel} from "@/models/flight.model.ts";
 
-const flights = ref<any>()
+const flights = ref<PageModel<FlightModel>>()
 axios.get('https://flight.pequla.com/api/flight?type=departure')
     .then(rsp => flights.value = rsp.data)
 </script>
@@ -10,17 +12,18 @@ axios.get('https://flight.pequla.com/api/flight?type=departure')
 <template>
 
   <div id="Departures">
-    {{ flights }}
-    <h1 class="h3">Dolasci</h1>
+<!--    {{ flights }}-->
+    <h1 class="container text-center"><i class="fa-solid fa-plane-departure"></i> Odlasci</h1>
     <hr>
-    <div v-if="flights">
-      <table class="table table-striped table-hover">
+    <div class="container" v-if="flights">
+      <table class="table table-striped table-hover text-center">
         <thead>
         <tr>
           <th scope="col">Broj leta</th>
-          <th scope="col">Destinacija</th>
-          <th scope="col">Kompanija</th>
-          <th scope="col">Vreme</th>
+          <th scope="col">Odredište</th>
+          <th scope="col">Izlaz</th>
+          <th scope="col">Planirano vreme</th>
+          <th scope="col">Procenjeno vreme</th>
           <th scope="col">Info</th>
         </tr>
         </thead>
@@ -28,8 +31,9 @@ axios.get('https://flight.pequla.com/api/flight?type=departure')
         <tr v-for="flight in flights.content" :flight-id="flight.id">
           <th scope="row">{{ flight.flightNumber }}</th>
           <td>{{ flight.destination }}</td>
-          <td>Wiz</td>
-          <td>3.4.2025. 20:00</td>
+          <td>{{ (flight.gate)? flight.gate : "N/A" }}</td>
+          <td>{{ new Date(flight.scheduledAt).toLocaleString('sr') }}</td>
+          <td>{{ flight.estimatedAt ? new Date(flight.estimatedAt).toLocaleString('sr') : 'N/A'}}</td>
           <td>
             <button type="button" class="btn btn-primary">A</button>
             <button type="button" class="btn btn-danger">B</button>
