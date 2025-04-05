33dcfccf487dd33edef5bba29aac7bc2e2b4ee64
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-06 00:04:47 +0200

    Tiny tweaks

--- frontend/src/views/DestinationsView.vue
+++ frontend/src/views/DestinationsView.vue
@@ -6,10 +6,6 @@ import type {PageModel} from "@/models/page.model.ts"
 
 const flights = ref<PageModel<FlightModel>>()
 
-// function destImg() {
-//   return 'https://img.pequla.com/destination/' + flight.value.destination.toLowerCase().split(' ')[0] + '.jpg'
-// }
-
 function toImg(destination: string) {
   return 'https://img.pequla.com/destination/' + destination.toLowerCase().split(' ')[0] + '.jpg'
 }
@@ -25,12 +21,29 @@ onMounted(() => retrieveData())
 
 <template>
   <br>
-  <div class="row px-5" v-if="flights">
-    <div class="card col-4 p-3 md-auto" v-for="flight in flights.content">
-      <img :src="toImg(flight.destination)" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title"></h5>
-        <p class="card-text">{{ flight.destination }}</p>
+  <div class="container px-4">
+    <div class="row gx-5" v-if="flights">
+      <div class="card col-3 p-3 md-auto" v-for="flight in flights.content">
+        <img :src="toImg(flight.destination)" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h3 class="card-title"></h3>
+          <p class="card-text text-center">{{ flight.destination }}</p>
+          <div class="container">
+
+            <div class="d-flex justify-content-between">
+                <!-- Odlazni letovi -->
+                <div class="flex-column" v-if="flights">
+                  <h6>Odlazni letovi</h6>
+                </div>
+
+              <!-- Dolazni letovi -->
+              <div class="flex-column">
+                <h6>Dolazni letovi</h6>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   </div>
