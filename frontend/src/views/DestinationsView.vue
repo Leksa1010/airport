f1ca4109b251980c535cadd661ae95dc1147a2d3
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-05 23:24:25 +0200

    Added cards for destiantions

--- frontend/src/views/DestinationsView.vue
+++ frontend/src/views/DestinationsView.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import {FlightModel} from '@/models/flight.model';
+import {FlightService} from '@/services/FlightService';
+import {onMounted, ref} from "vue";
+import type {PageModel} from "@/models/page.model.ts"
+
+const flights = ref<PageModel<FlightModel>>()
+
+// function destImg() {
+//   return 'https://img.pequla.com/destination/' + flight.value.destination.toLowerCase().split(' ')[0] + '.jpg'
+// }
+
+function toImg(destination: string) {
+  return 'https://img.pequla.com/destination/' + destination.toLowerCase().split(' ')[0] + '.jpg'
+}
+
+function retrieveData(page = 0) {
+  FlightService.getAllFlights(0)
+      .then(rsp => flights.value = rsp.data)
+}
+
+onMounted(() => retrieveData())
 
 </script>
 
 <template>
-  <p>Destinacije</p>
+  <br>
+  <div class="row px-5" v-if="flights">
+    <div class="card col-4 p-3 md-auto" v-for="flight in flights.content">
+      <img :src="toImg(flight.destination)" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">{{ flight.destination }}</p>
+      </div>
+    </div>
+  </div>
+
 </template>
