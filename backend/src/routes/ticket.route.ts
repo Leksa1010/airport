82beceed7a27c8b97ccaa71f592872288cdbf420
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-11 16:17:19 +0200

    Create ticket.route.ts

---
+++ backend/src/routes/ticket.route.ts
@@ -0,0 +1,15 @@
+import {Router} from "express";
+import {TicketService} from "../services/ticket.service";
+
+export const TicketRouter = Router()
+
+TicketRouter.get("/", async (req, res) => {
+    try{
+        res.json(await TicketService.getAllTickets())
+    } catch(e) {
+        res.status(500).json({
+            message: e.message,
+            timestamp: new Date() //Date.now()
+        })
+    }
+})
