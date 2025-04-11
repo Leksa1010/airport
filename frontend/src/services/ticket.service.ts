65ddd398949d490629cf352fa38f3324bdd4608e
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-11 18:21:12 +0200

    Fixed issues

--- frontend/src/services/ticket.service.ts
+++ frontend/src/services/ticket.service.ts
@@ -1,22 +1,21 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "http://localhost:3000/api",
     headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
+        "Accept": "application/json"
     },
     validateStatus: (status) => {
-        return status == 200 || status == 204
+        return status === 200 || status === 204
     }
 })
 
 export class TicketService {
     static async getAllTickets() {
-        return await client.get("/ticket");
+        return await client.get('/ticket')
     }
 
     static async getFlightById(id: number) {
-        return await client.get("/flight/" + id);
+        return await client.get('/flight/' + id)
     }
 }
