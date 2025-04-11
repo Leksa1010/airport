21554eb6e3b72fdc1f7aee12b1172a8455671e7d
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-11 16:21:41 +0200

    Create flight.service.ts

---
+++ backend/src/services/flight.service.ts
@@ -0,0 +1,13 @@
+const client = axios.create({
+    baseURL: "https://flight.pequla.com/api/flight",
+    headers: {
+        "Accept": "application/json",
+    }
+})
+
+export class FlightService {
+    static async getFlightById(id: number) {
+        const rsp = await client.get(`/${id}`)
+        return rsp.data
+    }
+}
