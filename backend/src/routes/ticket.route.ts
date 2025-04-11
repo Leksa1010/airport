7393e323ce9a82a9db2ae63165e75edb6b8f7053
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-11 16:59:37 +0200

    Worked on ticket logic

--- backend/src/routes/ticket.route.ts
+++ backend/src/routes/ticket.route.ts
@@ -13,3 +13,27 @@ TicketRoute.get("/", async (req, res) => {
         })
     }
 })
+
+TicketRoute.post("/", async (req, res) => {
+    try {
+        res.json(await TicketService.createTicket(req.body))
+    } catch(e) {
+        res.status(500).json({
+            message: e.message,
+            timestamp: new Date()
+        })
+    }
+})
+
+TicketRoute.delete("/:id", async (req, res) => {
+    try {
+        const id = Number.parseInt(req.params.id)
+        res.json(await TicketService.deleteTicket(id))
+        res.status(204).send()
+    } catch(e) {
+        res.status(500).json({
+            message: e.message,
+            timestamp: new Date()
+        })
+    }
+})
--- backend/src/services/ticket.service.ts
+++ backend/src/services/ticket.service.ts
@@ -1,12 +1,13 @@
 import {AppDataSource} from "../db";
 import {Ticket} from "../entities/Ticket";
 import {IsNull} from "typeorm";
+import {isNull} from "node:util";
 
 const repo = AppDataSource.getRepository(Ticket)
 
 export class TicketService {
     static async getAllTickets() {
-        return repo.find({
+        const data = await repo.find({
             where: {
                 deletedAt: IsNull()
             },
@@ -14,5 +15,33 @@ export class TicketService {
                 airline: true
             }
         })
+
+        data.forEach(e => delete e.deletedAt)
+        return data
+    }
+
+    static async createTicket(ticket: Ticket) {
+        ticket.userId = 1
+        ticket.deletedAt = null
+        ticket.createdAt = new Date()
+
+        const data = await repo.save(ticket)
+        delete data.deletedAt
+        return data
+    }
+
+    static async deleteTicket(id: number) {
+        const data = await repo.findOne({
+            where: {
+                ticketId: id,
+                deletedAt: IsNull()
+            }
+        })
+
+        if (data == undefined)
+            throw new Error("Karta nije pronađena")
+
+        data.deletedAt = new Date()
+        await repo.save(data)
     }
 }
