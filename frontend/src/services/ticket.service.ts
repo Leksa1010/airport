0596eec7cb439cfa6afcfbf97b4a98f7e294551e
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-11 17:43:32 +0200

    Worked on ticket logic for FE

---
+++ frontend/src/services/ticket.service.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const client = axios.create({
+    baseURL: "http://localhost:3000/api/",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    validateStatus: (status) => {
+        return status == 200 || status == 204
+    }
+})
+
+export class TicketService {
+    static async getAllTickets() {
+        return await client.get("/ticket");
+    }
+
+    static async getFlightById(id: number) {
+        return await client.get("/flight/" + id);
+    }
+}
--- frontend/src/views/TicketView.vue
+++ frontend/src/views/TicketView.vue
@@ -1,4 +1,16 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import {TicketService} from "@/services/ticket.service.ts";
+
+const tickets = ref<any>()
+TicketService.getAllTickets().then(rsp => {
+  tickets.value = rsp.data
+  tickets.value.forEach((ticket: any) => {
+    TicketService.getFlightById(ticket.flight_id).then(rsp => {
+      ticket.flight = rsp.data;
+    })
+  })
+})
 
 </script>
 
