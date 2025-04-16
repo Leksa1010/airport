0a0dbcf4c1409ecf97f059c0255686485ad8c7ed
Leksa1010 <127348571+Leksa1010@users.noreply.github.com>
2025-04-16 15:21:24 +0200

    Update index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -24,58 +24,7 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes, /*: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-        {
-            path: '/about',
-            name: 'about',
-            component: () => import('../views/AboutView.vue'),
-        },
-        {
-            path: '/arrivals',
-            name: 'arrivals',
-            component: () => import('../views/ArrivalsView.vue'),
-        },
-        {
-            path: '/departures',
-            name: 'departures',
-            component: () => import('../views/DeparturesView.vue'),
-        },
-        {
-            path: '/destinations',
-            name: 'destinations',
-            component: () => import('../views/DestinationsView.vue'),
-        },
-        {
-            path: '/destination/:id',
-            name: 'destinationbrowser',
-            component: () => import('../views/DestinationBrowserView.vue'),
-        },
-        {
-            path: '/flight/:id',
-            name: 'flight',
-            component: () => import('../views/FlightView.vue'),
-        },
-        {
-            path: '/flight/:id/book',
-            name: 'book',
-            component: () => import('../views/BookView.vue'),
-        },
-        {
-            path: '/ticket',
-            name: 'ticket',
-            component: () => import('../views/TicketView.vue'),
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('../views/LoginView.vue'),
-        }
-    ],*/
+    routes
 })
 
 router.beforeEach((to, from, next) => {
